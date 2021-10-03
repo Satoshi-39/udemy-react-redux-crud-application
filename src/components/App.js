de23c3af21db70afe60a3b7c2165fe295218c8d2
Satoshi-39 <88920771+Satoshi-39@users.noreply.github.com>
2021-10-03 14:43:20 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import { connect } from "react-redux"
+import { increment, decrement } from "../actions"
 // import PropTypes from "prop-types";
 
 // class App extends Component {
@@ -71,39 +73,62 @@ import React, {Component} from "react";
 
 
 //ステート
-const App = () => (<Counter></Counter>)
+//const App = () => (<Counter></Counter>)
   
-class Counter extends Component {
-  //コンストラクター
-constructor(props){
-  super(props)
-  console.log(this.state)
-  this.state = { count: 0 }
-}
+//class Counter extends Component {
+  //コンストラクター/reduxではreducerが行うので不要!?
+// constructor(props){
+//   super(props)
+//   console.log(this.state)
+//   this.state = { count: 0 }
+// }
 
-//1プラスする処理
-handlePlusButton = () => {
+//1プラスする処理/reduxでは別で行うから不要
+//handlePlusButton = () => {
   //下記一行はやってはいけない 状態を直接いじってはいけない
   //this.state = {count: this.state.count + 1}
-  this.setState({count: this.state.count + 1})
-}
+//   this.setState({count: this.state.count + 1})
+// }
 
-handleMinusButoon = () => {
-  this.setState({count: this.state.count -1})
-}
+//1マイナスする処理/reduxでは別で行うから不要
+// handleMinusButoon = () => {
+//   this.setState({count: this.state.count -1})
+// }
 
+// render(){
+//   //ステートの中にアクセス
+//   //returnで返すjsxは親が一つでないといけない
+//   return (
+//     <React.Fragment>
+//       <div>count: {this.state.count}</div>
+//       <button onClick={this.handlePlusButton}>+1</button>
+//       <button onClick={this.handleMinusButoon}>-1</button>
+//     </React.Fragment>
+//     )
+//   }
+// }
+
+//reduxでのクラス
+class App extends Component {
 render(){
-  //ステートの中にアクセス
+  const props = this.props
   //returnで返すjsxは親が一つでないといけない
   return (
     <React.Fragment>
-      <div>count: {this.state.count}</div>
-      <button onClick={this.handlePlusButton}>+1</button>
-      <button onClick={this.handleMinusButoon}>-1</button>
+      <div>value: { props.value }</div>
+      <button onClick={props.increment}>+1</button>
+      <button onClick={props.decrement}>-1</button>
     </React.Fragment>
     )
   }
 }
 
+const mapStateToProps = state => ({ value: state.count.value })
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+})
+//上記はこのようにも書ける
+//const mapDispatchToProps = ({ increment, decrement　})
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
