c7794e4f0242301caa86afc86515ab7a01c0a9d9
Satoshi-39 <88920771+Satoshi-39@users.noreply.github.com>
2021-10-03 14:00:12 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+//下記はstoreの作成
+import { createStore } from "redux"
+//下記は全コンポーネントに渡す特殊なredux
+//これを使うことで全ての親から子へpropsを全部渡す必要がなくなった
+import　{ Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+//前回作成したreducerをインポート
+import reducer from "./reducers";
+import App from './components/App';
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
